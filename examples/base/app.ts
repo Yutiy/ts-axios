e5458e6739447a246924900f039a69386f9c1fed
yanglin <494657028@qq.com>
2019-11-08 00:32:56 +0800

    feat: parse headers

---
+++ examples/base/app.ts
@@ -0,0 +1,104 @@
+import axios from '../../src/index'
+
+// get query
+// --------------------------------------------------------------------
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: ['bar', 'baz']
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: {
+      bar: 'baz'
+    }
+  }
+})
+
+const date = new Date()
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    date
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    foo: '@:$, '
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get',
+  params: {
+    baz: 'bar',
+    foo: null,
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get#hash',
+  params: {
+    foo: 'baz'
+  }
+})
+
+axios({
+  method: 'get',
+  url: '/base/get?baz=foo',
+  params: {
+    foo: 'bar'
+  }
+})
+
+
+// post data
+// --------------------------------------------------------------------
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const arr = new Int32Array([21, 31])
+axios({
+  method: 'post',
+  url: '/base/buffer',
+  data: arr
+})
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  headers: {
+    'content-type': 'application/json;charset=utf-8',
+    'Accept': 'application/json, text/plain, */*'
+  },
+  data: {
+    a: 1,
+    b: 2
+  }
+})
+
+const paramsString = 'q=URLUtils.searchParams&topic=api'
+const searchParams = new URLSearchParams(paramsString)
+
+axios({
+  method: 'post',
+  url: '/base/post',
+  data: searchParams
+})
--- examples/server.js
+++ examples/server.js
@@ -15,6 +15,27 @@ router.get('/simple/get', function (req, res) {
   })
 })
 
+router.get('/base/get', function (req, res) {
+  res.json(req.query);
+})
+
+router.post('/base/post', function(req, res) {
+  res.json(req.body)
+})
+
+router.post('/base/buffer', function(req, res) {
+  let msg = []
+  req.on('data', (chunk) => {
+    if (chunk) {
+      msg.push(chunk)
+    }
+  })
+  req.on('end', () => {
+    let buf = Buffer.concat(msg)
+    res.json(buf.toJSON())
+  })
+})
+
 app.use(webpackDevMiddleware(compiler, {
   publicPath: '/__build__/',
   stats: {
